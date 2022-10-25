6d24cb55c6056793e6d60be309f624f8e46f08f5
teustenn <teustenn@gmail.com>
2022-10-24 23:25:07 -0300

    Product

---
+++ src/main/java/com/sentinela/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.sentinela.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sentinela.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
