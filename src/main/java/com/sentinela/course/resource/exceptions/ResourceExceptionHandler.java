fc70046d1a9278e03a5de05173ff44e1cdfb912f
teustenn <teustenn@gmail.com>
2022-10-26 23:28:31 -0300

    Exception Handling - delete

--- src/main/java/com/sentinela/course/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/sentinela/course/resource/exceptions/ResourceExceptionHandler.java
@@ -16,8 +16,15 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-		String error = "Resource Not Found!";
+		String error = "Resource NOT FOUND!";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError er = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(er);
+	}
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database ERROR!";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError er = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(er);
 	}
